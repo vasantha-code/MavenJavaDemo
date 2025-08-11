e76eb4a2da3d0ce1b7b062b050b9ba513a90f73a
vasantha-code <vasantha12.kmit@gmail.com>
2025-08-12 04:21:11 +0530

    added maven java project

---
+++ MavenJavaDemo/src/main/java/SE/MavenJavaDemo/App.java
@@ -0,0 +1,13 @@
+package SE.MavenJavaDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is Maven Java Project" );
+    }
+}
